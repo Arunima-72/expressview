fc9d660c9216185b655b0490782992e0d730d90f
Arunima-72 <arunimasuresh89@gmail.com>
2025-04-09 14:10:39 +0530

    first commit

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express=require('express');
 const app=new express();
 const morgan=require('morgan');
 app.use(morgan('dev'));
-const routes=require('./routes/routes');
-app.use('/basic',routes);//Redirecting to routes
+
+// app.use('/basic',routes);//Redirecting to routes
 app.set('view engine','ejs');
 app.use(express.static('public'));
+const nav=[
+    
+       {name:'Home',link:'/basic'},
+       {name:'Add Student',link:'/basic/addform'}
+    
+]
+const routes=require('./routes/routes')(nav);
+app.use('/basic',routes);
 app.set("views",__dirname+'/view');//views &__dirname is keyword and view is filename
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -6,41 +6,49 @@ router.use(express.urlencoded({extended:true}))
 const data=[{"name":"Anu","age":22,"place":"Kollam"},
     {"name":"Ammu","age":23,"place":"Kochi"},
     {"name":"Archa","age":24,"place":"Kollam"}
-        ]
+]
+  function route(nav){    
 router.get('/',(req,res)=>{
     res.render("home",{
        title:"My page" ,
        my_name:"Anu",
-       data
+       data,
+       nav
     })
     
 })
 
     router.get('/addform',(req,res)=>{
         //res.send(data);
-        res.render("studentform");
+        res.render("studentform",{nav})
+        
 })
 router.post('/add',(req ,res)=>{
 //console.log(req.body)
 data.push(req.body);
 res.redirect('/basic');
 })
-router.put('/edit/:id',(req ,res)=>{
+router.post('/edit/:id',(req ,res)=>{
     const idno=req.params.id;
     data.splice(idno,1,req.body);
-    res.send(data);
+    // res.send(data);
+    res.redirect('/basic');
+   
 })
-router.delete('/remove/:id',(req,res)=>{
+router.get('/delete/:id',(req,res)=>{
     data.splice(req.params.id,1);
-    res.send(data);
+    res.redirect('/basic');
+    
+})
+router.get('/update/:ind',(req,res)=>{
+res.render("updateform",{nav,id:req.params.ind,data});
+
 })
 
+return router
+
+  }
 
 
 
-
-
-
-
-
-module.exports=router
+module.exports=route
